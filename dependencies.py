a0e5dcda4a21dd1fb1eccf430c1eab49fd6f4fd7
papadonut9 <47145773+papadonut9@users.noreply.github.com>
2019-08-06 04:37:35 +0530

    Create dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,8 @@
+#Virtual environment creation so that installed libraries don't get used globally
+pip3 install virtualenv
+
+#Flask framework
+pip3 install flask
+
+#datetime library to timestamp the notes, this will be already provided in python. But if you have OS provided Python distro, you need this
+pip3 install datetime
